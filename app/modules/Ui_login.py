93462d02f69aa70a0320e39e448a760e529c32cd
SISUBEN <100661067+SISUBEN@users.noreply.github.com>
2024-07-14 20:42:36 +0800

    fixed translate

--- app/modules/Ui_login.py
+++ app/modules/Ui_login.py
@@ -18,6 +18,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
     QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout,
     QWidget)
+import resources_rc
+import resources_rc
+import resources_rc
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -35,20 +38,21 @@ class Ui_Form(object):
         self.label_3.setPixmap(QPixmap(u":/images/images/login.png"))
         self.layoutWidget = QWidget(Form)
         self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(810, 40, 541, 591))
+        self.layoutWidget.setGeometry(QRect(740, 30, 682, 591))
         self.verticalLayout_2 = QVBoxLayout(self.layoutWidget)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
         self.label_4 = QLabel(self.layoutWidget)
         self.label_4.setObjectName(u"label_4")
-        self.label_4.setMaximumSize(QSize(16777215, 300))
+        self.label_4.setMaximumSize(QSize(640, 200))
         font = QFont()
         font.setFamilies([u"Consolas"])
-        font.setPointSize(23)
+        font.setPointSize(28)
         font.setBold(True)
         self.label_4.setFont(font)
         self.label_4.setStyleSheet(u"color: white;")
         self.label_4.setTextFormat(Qt.RichText)
+        self.label_4.setAlignment(Qt.AlignCenter)
 
         self.verticalLayout_2.addWidget(self.label_4)
 
@@ -56,6 +60,10 @@ class Ui_Form(object):
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_5)
+
         self.label = QLabel(self.layoutWidget)
         self.label.setObjectName(u"label")
         self.label.setStyleSheet(u"color: white;")
@@ -69,6 +77,10 @@ class Ui_Form(object):
 
         self.horizontalLayout.addWidget(self.username)
 
+        self.horizontalSpacer_4 = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_4)
+
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
@@ -78,6 +90,10 @@ class Ui_Form(object):
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalSpacer_6 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_6)
+
         self.label_2 = QLabel(self.layoutWidget)
         self.label_2.setObjectName(u"label_2")
         self.label_2.setStyleSheet(u"color: white;")
@@ -87,10 +103,15 @@ class Ui_Form(object):
 
         self.password = QLineEdit(self.layoutWidget)
         self.password.setObjectName(u"password")
+        self.password.setMaximumSize(QSize(500, 16777215))
         self.password.setStyleSheet(u"background-color: white;")
         self.password.setEchoMode(QLineEdit.Password)
 
         self.horizontalLayout_2.addWidget(self.password)
+
+        self.horizontalSpacer_3 = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_3)
 
 
         self.verticalLayout.addLayout(self.horizontalLayout_2)
@@ -130,11 +151,13 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Login", None))
         self.label_3.setText("")
-        self.label_4.setText(QCoreApplication.translate("Form", u"Welcome To Access H.D.D System", None))
-        self.label.setText(QCoreApplication.translate("Form", u"Username:", None))
-        self.label_2.setText(QCoreApplication.translate("Form", u"Password:", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"\u6b22\u8fce\u8bbf\u95eeH.D.D System", None))
+        self.label.setText(QCoreApplication.translate("Form", u"\u7528\u6237\u540d\uff1a", None))
+        self.username.setPlaceholderText(QCoreApplication.translate("Form", u"\u8bf7\u8f93\u5165\u7528\u6237\u540d", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"\u5bc6   \u7801\uff1a", None))
         self.password.setInputMask("")
-        self.login_btn.setText(QCoreApplication.translate("Form", u"Login", None))
-        self.register_2.setText(QCoreApplication.translate("Form", u"Register", None))
+        self.password.setPlaceholderText(QCoreApplication.translate("Form", u"\u8bf7\u8f93\u5165\u5bc6\u7801", None))
+        self.login_btn.setText(QCoreApplication.translate("Form", u"\u767b\u5165", None))
+        self.register_2.setText(QCoreApplication.translate("Form", u"\u6ce8\u518c", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -3,21 +3,15 @@ from PySide6.QtWidgets import (
     QWidget,
     QMessageBox
 )
-from PySide6.QtCore import (
-    Signal,
-    Qt
-)
 from app.modules.Ui_login import Ui_Form
 from app.assets.resources_rc import *
 from qt_material import apply_stylesheet
-class MainWindow(QWidget, Ui_Form):
-    clicked = Signal()
+class LoginWindow(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.login_btn.clicked.connect(self.login)
-        self.register_2.clicked.connect(self.register_2)
-        
+        self.register_2.clicked.connect(self.register)
     def login(self):
         if self.username.text() == "admin" and self.password.text() == "123456":
             self.msgBox("Login Success", f"Welcome! {self.username.text()}")
@@ -27,12 +21,28 @@ class MainWindow(QWidget, Ui_Form):
         MessageBox = QMessageBox()
         MessageBox.about(self, title, text)
         
-    def register():
+    def register(self):
         pass
     
+    def openMainWindow(self):
+        self.close()
+        self.mainWindow = MainWindow()
+        self.mainWindow.show()
+        
+    def openRegisterWindow(self):
+        self.close()
+        # self.registerWindow = RegisterWindow()
+        # self.registerWindo.show()
+
+class MainWindow(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle("test")
+        
+
 if __name__ == "__main__":
     app = QApplication([])
-    apply_stylesheet(app, theme="light_blue.xml")
-    window = MainWindow()
+    apply_stylesheet(app, theme="dark_blue.xml")
+    window = LoginWindow()
     window.show()
     app.exec()
