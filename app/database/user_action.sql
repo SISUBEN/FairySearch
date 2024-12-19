2d5db0340fe8483007fbc62f6c28e79128dba2a7
SISUBEN <100661067+SISUBEN@users.noreply.github.com>
2024-12-19 22:56:44 +0800

    add video activity data table

---
+++ app/database/user_action.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user_action (
+    user_id INT NOT NULL,
+    video_id INT NOT NULL,
+    is_liked BOOLEAN NOT NULL,
+    is_favourite BOOLEAN NOT NULL,
+    is_coin BOOLEAN NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+);
---
+++ app/database/users/users.py
@@ -0,0 +1,207 @@
+import sqlite3
+from db_config import Config
+from app.utils.crypto import CryptoHasher
+from app.modules.logger.logger import logger
+cryptor = CryptoHasher()
+config = Config
+
+class Userdb:
+        def __init__(self) -> None:
+            self.user_connect = sqlite3.connect(config.USER_DB)
+            self.user_cur = self.user_connect.cursor()
+            self.init_userdb()
+
+        def init_userdb(self) -> None:
+            try:
+                self.user_connect.execute(config.USER_SQL)
+                self.user_connect.commit()
+            except sqlite3.Error as e:
+                logger.debug(f"init user db fail: {e}")
+                self.user_connect.rollback()
+        def user_add(self, username: str, password: str) -> None:
+            try:
+                self.user_connect.execute(config.USER_ADD, (username, password))
+                self.user_connect.commit()
+            except sqlite3.Error as e:
+                logger.debug(f"add user fail: {e}")
+                self.user_connect.rollback()
+
+        def t_user_add(self, username: str, password: str) -> str:
+            """Add user to database with token
+
+            Args:
+                username (str): username
+                password (str): password must be plain text
+            """
+            try:
+                encrypted = cryptor.sha256(password)
+                token = self.generate_token(username, encrypted)
+                self.user_connect.execute(config.T_USER_ADD, (username, encrypted, token))
+                self.user_connect.commit()
+                return token
+            except sqlite3.Error as e:
+                logger.debug(f"add user token fail: {e}")
+                self.user_connect.rollback()
+            except Exception as e:
+                logger.debug(f"create token fail: {e}")
+            
+        def verify_token(self, token: str) -> bool:
+            """Verify token
+
+            Args:
+                token (str): token
+
+            Returns:
+                bool: is True if token is valid
+            """
+            try:
+                return self.user_connect.execute(config.T_USER_QUERY, (token,)).fetchone() is not None
+            except sqlite3.Error as e:
+                logger.debug(f"verify token error: {e}")
+
+        def get_token(self, username: str) -> str:
+            """Get token by username
+
+            Args:
+                username (str): username
+
+            Returns:
+                str: token
+                
+            Exception:
+                sqlite3.Error: if error occurs
+            """
+            try:
+                 return self.user_connect.execute(config.T_USER_QUERY, (username,)).fetchone()[0][0]
+            except sqlite3.Error as e:
+                logger.debug(f"get token error: {e}")
+        
+        
+        def generate_token(self, username: str, password: str) -> str:
+            """Generate token for user
+
+            Args:
+                username (str): username plain text
+                password (str): password cipher text
+
+            Raises:
+                Exception: if user not exists
+
+            Returns:
+                str[64]: token (a sha256 hash)
+            """
+            token = {
+                "username": username,
+                "pasword": password, 
+            }
+            token = str(token)
+            return cryptor.sha256(token)
+                
+        
+        def user_exists(self, username: str) -> bool:
+            """Check if user exists
+
+            Args:
+                username (str): username
+
+            Returns:
+                bool: is Ture if user exists
+                
+            Exception:
+                sqlite3.Error: if error occurs
+            """
+            try: 
+                return (
+                    self.user_connect.execute(config.USER_QUERY, (username,)).fetchone()
+                    is not None
+                ) # if query result is not None, return True, else return result
+            except sqlite3.Error as e:
+                logger.debug(f"An error occurred when querying user exists: {e}")
+        
+        def query_username(self, token: str) -> list:
+            """query username by token
+
+            Args:
+                token (str): login token
+
+            Returns:
+                str: username
+            """
+            try:
+                return self.user_connect.execute(config.T_USERNAME_QUERY, (token,)).fetchall()[0][0]
+            except sqlite3.Error as e:
+                logger.debug(f"query username error: {e}")
+
+        def destroy_db(self, db_name: str) -> None:
+            try:
+                self.user_connect.execute(f"DROP TABLE IF EXISTS {db_name};")
+                self.user_connect.commit()
+            except sqlite3.Error as e:
+                logger.debug(f"destroy db fail: {e}")
+                
+        def query_user_password(self, username: str) -> str:
+            """Query user password by username
+
+            Args:
+                username (str): username
+
+            Returns:
+                str: user password
+            """
+            logger.debug(f"self.user_connect.execute(config.USER_QUERY_PWD, (username,)).fetchall() = {self.user_connect.execute(config.USER_QUERY_PWD, (username,)).fetchall()}")
+            try:
+                return self.user_connect.execute(config.USER_QUERY_PWD, (username,)).fetchall()[0][0]
+            except sqlite3.Error as e:
+                logger.debug(f"query user password error: {e}")
+                
+        def query_user_uid(self, username: str) -> int:
+            """Query user uid by username
+
+            Args:
+                username (str): username
+
+            Returns:
+                int : user uid
+                
+            Exception:
+                sqlite3.Error: if query fail
+            """
+            try:
+                return self.user_connect.execute(config.USER_QUERY_UID, (username,)).fetchall()[0][0]
+            except sqlite3.Error as e:
+                logger.debug(f"query user uid error: {e}")
+
+        def query_uid(self, token: str) -> int:
+            """Query user uid by token
+
+            Args:
+                token (str): login token
+
+            Returns:
+                int: user uid
+                
+            Exception:
+                sqlite3.Error: if query fail
+            """
+            try:
+                return self.user_connect.execute(config.T_USER_QUERY_UID, (token,)).fetchall()[0][0]
+            except sqlite3.Error as e:
+                logger.debug(f"query uid error: {e}")
+        
+        def verify_user(self, password: str, username: str) -> bool:
+            """Verfy user password
+
+            Args:
+                password (str): user password (encrypted)
+                username (str): username
+
+            Returns:
+                bool: True if password is correct, False otherwise
+            """
+            logger.debug(f"pwd: {password}, username: {username}")
+            logger.debug(f"Is user exists? => {self.user_exists(username)}")
+            logger.debug(f"self.user_connect.execute(config.USER_QUERY_PWD, (username,)).fetchall() = {self.user_connect.execute(config.USER_QUERY_PWD, (username,)).fetchall()}")
+            return self.query_user_password(username) == password
+
+        def __del__(self) -> None:
+            self.user_connect.close()
