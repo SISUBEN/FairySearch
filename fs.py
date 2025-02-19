59e842c78b38af8b514300b0edfc3b5cb58cad99
SISUBEN <100661067+SISUBEN@users.noreply.github.com>
2025-02-19 17:33:27 +0800

    i18nize

--- fs.py
+++ fs.py
@@ -1,8 +1,21 @@
 from app.__init__ import *
 from app.libs.login import LoginWindow
+import gettext
+import threading
+
+# thread local storage
+thread = threading.local()
+
+
+def set_lang(language: str):
+    thread.trans = gettext.translation('app', localedir='locales', languages=[language])
+
+def translate(text: str):
+    return thread.trans.gettext(text)
 
 if __name__ == "__main__":
     try:
+        set_lang("zh-CN")
         console = Console()
         console.print(Rule("Initializing"))
         app = QApplication([])
@@ -11,8 +24,7 @@ if __name__ == "__main__":
         loginWindow.show()
         app.exec()
     except Exception as err:
-        logger.critical(
-            f"An error occurred while the program was running: {err}")  
+        logger.critical(f"An error occurred while the program was running: {err}")
         app.shutdown()
     except KeyboardInterrupt:
         logger.error("The program was interrupted by the user")
